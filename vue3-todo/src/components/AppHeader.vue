c2c89d0b3e341d29eeada9d2d8a4dff34e3d7f75
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-09-28 17:10:01 +0100

    refactored header component, added elements

--- vue3-todo/src/components/AppHeader.vue
+++ vue3-todo/src/components/AppHeader.vue
@@ -1,40 +1,55 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand h4 text-primary" href="#">Todo App</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/login" v-if="!authStore.isAuthenticated"
+              >Login</router-link
+            >
+          </li>
+
+          <li class="nav-item">
+            <router-link class="nav-link" to="/register" v-if="!authStore.isAuthenticated"
+              >Register</router-link
+            >
+          </li>
+
+          <li class="nav-item" v-if="authStore.isAuthenticated">
+            <router-link class="nav-link" to="/todos">My Todos</router-link>
+          </li>
+
+          <li class="nav-item" v-if="authStore.isAuthenticated">
+            <button @click="logout" class="btn btn-primary">
+              <router-link to="/logout" class="text-white">Logout</router-link>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-    </nav>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
-<script>
-export default {
-    setup () {
-        
+<script setup>
+import useUserStore from '../stores/users'
 
-        return {}
-    }
+const authStore = useUserStore()
+
+const logout = () => {
+  authStore.logout()
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
