191437e3ef7932aa0ada283db25fc683ac3f5f57
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-09-28 17:10:33 +0100

    finished working on login view

---
+++ vue3-todo/src/views/Auth/LoginView.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="">
+    <div>
+      <h1 class="text-success">Login your Account</h1>
+    </div>
+    <form @submit.prevent="login">
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input
+          type="email"
+          v-model="email"
+          class="form-control"
+          id="login-email"
+          aria-describedby="emailHelp"
+          placeholder="example@example.com"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input
+          type="password"
+          v-model="password"
+          class="form-control"
+          id="login-password"
+          placeholder="************"
+        />
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+
+      <div class="row">
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import useUserStore from '../../stores/users'
+import { useRouter } from 'vue-router'
+
+const email = ref('')
+const password = ref('')
+
+const authStore = useUserStore()
+const router = useRouter()
+
+const login = async () => {
+  try {
+    await authStore.login(email.value, password.value)
+    router.push('/todos')
+  } catch (error) {
+    console.log(error)
+  }
+}
+</script>
