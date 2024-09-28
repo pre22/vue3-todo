7d450c16019da93ea1762ea071138cc3760d2813
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-09-28 17:10:15 +0100

    created new routes

--- vue3-todo/src/router/index.js
+++ vue3-todo/src/router/index.js
@@ -1,23 +1,60 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import RegisterView from '@/views/Auth/RegisterView.vue'
+import LoginView from '@/views/Auth/LoginView.vue'
+import TodoView from '@/views/TodoView.vue'
+import HomeView from '@/views/HomeView.vue'
+import useUserStore from '@/stores/users'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterView
+  },
+  {
+    path: '/todos',
+    name: 'todo',
+    component: TodoView,
+    meta: {
+      requiresAuth: true
+    }
+
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home'}
+  }
+  
+]
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes,
+})
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta.requiresAuth) {
+    next()
+    return
+  }
+  const store = useUserStore()
+
+  if (store.user) {
+    next()
+  } else {
+    next({ name: 'home' })
+  }
 })
 
 export default router
