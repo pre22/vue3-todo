0a289024d8cbddcc0261b1553f5b309fcaace0a8
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-09-29 09:08:04 +0100

    createed user model

--- todo_api/users/models.py
+++ todo_api/users/models.py
@@ -1,3 +1,19 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser, PermissionsMixin
+from utils.models import BaseModel
+from .manager import CustomUserManager
 
-# Create your models here.
+class User(AbstractUser):
+    '''User Model'''
+    username = None
+    email = models.EmailField(unique=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.email}"
+
