7710c98423e09ee77923bcf6c4c2c1d1090203f7
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-09-29 11:46:02 +0100

    created choices  for todo

--- todo_api/todos/models.py
+++ todo_api/todos/models.py
@@ -1,12 +1,19 @@
 import uuid
 from django.db import models
 from utils.models import BaseModel
+from users.models import User
 
+
+TODO_STATUS = (
+    ("Completed", "Completed"),
+    ("Not-Completed", "Not-Completed"),
+)
 
 class TodoModel(BaseModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     todo = models.TextField()
-    status = models.CharField(max_length=50)
+    status = models.CharField(max_length=50, choices=TODO_STATUS, default="Not-Completed")
 
     def __str__(self):
         return f"{self.todo}"
