f82c86389679277af8799f7a6e2135a85fa92b00
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-09-29 09:50:12 +0100

    creating todo model

--- todo_api/todos/models.py
+++ todo_api/todos/models.py
@@ -1,3 +1,7 @@
+import uuid
 from django.db import models
+from utils.models import BaseModel
 
-# Create your models here.
+
+class TodoModel(BaseModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
