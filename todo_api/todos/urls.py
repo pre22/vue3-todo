9cc86bf7446ea6c0c3d3fdecc59c5be6d0cc6c86
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-09-29 11:46:24 +0100

    updated update url

--- todo_api/todos/urls.py
+++ todo_api/todos/urls.py
@@ -3,11 +3,9 @@ from . import views
 
 app_name = 'todos'
 
-url_patterns = [
-    path('login/', views.UserLoginView.as_view()),
-    path('register/', views.UserRegistrationView.as_view()),
+urlpatterns = [
     path('delete-todo/<str:id>/', views.DeleleTodoView.as_view()),
     path('get-todos/', views.TodosView.as_view()),
     path('add-todos/', views.TodosView.as_view()),
-    path('update-todos/', views.TodosView.as_view()),
+    path('update-todos/<str:id>/<str:status>/', views.TodosView.as_view()),
 ]
