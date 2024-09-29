5fca8b1d652af3abb1ff87c1af1b97913c510a56
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-09-29 09:54:28 +0100

    moved class to a new file

--- todo_api/todos/serializers.py
+++ todo_api/todos/serializers.py
@@ -1,13 +1,6 @@
 from rest_framework import serializers
 
 
-class UserAuthSerializer(serializers.Serializer):
-    '''
-    Serializer for Login and Registration
-    '''
-    email = serializers.EmailField(required=True)
-    password = serializers.CharField(max_length=200, required=True)
-
 class AddTodoSerializer(serializers.Serializer):
     '''
     Create New Todo
--- todo_api/todos/views.py
+++ todo_api/todos/views.py
@@ -4,53 +4,7 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from users.models import User
 from . import serializers
-
-
-
-class UserRegistrationView(APIView):
-    permission_classes = (permissions.AllowAny,)
-
-    def post(self, request, *args, **kwargs):
-
-        serializer = serializers.UserAuthSerializer(data=request.data)
-
-        if serializer.is_valid():
-            data = serializer.validated_data
-            user = User.objects.create(email=data['email'])
-            user.set_password(data['password'])
-            user.save()
-            return Response({'success': 'User Registration Success'}, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-
-class UserLoginView(APIView):
-    permission_classes = (permissions.AllowAny,)
-
-    def post(self, request, *args, **kwargs):
-
-        serializer = serializers.UserAuthSerializer(data=request.data)
-
-        if serializer.is_valid():
-            data = serializer.validated_data
-            
-            try:
-                user = User.objects.get(email=data['email'])
-                check = user.check_password(data['password'])
-                if check:
-                    refresh_token = RefreshToken.for_user(user)
-                    access_token = refresh_token.access_token
-                    response_data = {
-                        'refresh_token': str(refresh_token), 
-                        'access_token': str(access_token),
-                        'user': user.email
-                    }
-                    return Response(response_data, status=status.HTTP_201_CREATED)
-
-            except User.DoesNotExist:
-                return Response({'error': 'New User? Kindly Create a New Account'}, status=status.HTTP_400_BAD_REQUEST)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+from .models import TodoModel
 
 
 class TodosView(APIView):
