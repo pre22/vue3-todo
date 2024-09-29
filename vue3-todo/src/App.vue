4cfe20414e7b050ee63d0f2fc0a76c4891058ebc
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-09-29 20:36:40 +0100

    commented lifecycle hooks

--- vue3-todo/src/App.vue
+++ vue3-todo/src/App.vue
@@ -6,7 +6,22 @@
 </template>
 
 <script setup>
+// import { onMounted } from 'vue'
+// import { useRouter } from 'vue-router'
 import AppHeader from './components/AppHeader.vue'
+// import useUserStore from './stores/users'
+
+// const authStore = useUserStore()
+// const router = useRouter()
+
+// onMounted(() => {
+//   console.log(authStore.token)
+//   const token = localStorage.getItem('token')
+
+//   if (!token) {
+//     router.push('/login')
+//   }
+// })
 </script>
 
 <style scoped>
