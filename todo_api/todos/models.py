db5b66ec089f098491497c50b784e7239e203ac6
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-09-29 10:16:54 +0100

    updated todo model

--- todo_api/todos/models.py
+++ todo_api/todos/models.py
@@ -5,3 +5,8 @@ from utils.models import BaseModel
 
 class TodoModel(BaseModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    todo = models.TextField()
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.todo}"
