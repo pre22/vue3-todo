6bfc442391c269ebd0b91f4a2efc62d07552baf6
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-09-29 20:51:42 +0100

    Update TodoView.vue

--- vue3-todo/src/views/TodoView.vue
+++ vue3-todo/src/views/TodoView.vue
@@ -3,6 +3,18 @@
     <h1>Vue 3 Todo List</h1>
   </div>
 
+  <div class="alert alert-danger alert-dismissible fade show" role="alert" v-if="errorMessage">
+    <i class="fa-solid fa-triangle-exclamation"></i>
+    {{ errorMessage }}
+    <button
+      type="button"
+      class="btn-close"
+      data-bs-dismiss="alert"
+      aria-label="Close"
+      @click="clearErrorMessage"
+    ></button>
+  </div>
+
   <div class="row">
     <div class="col">
       <div class="input-group mb-3">
@@ -19,11 +31,11 @@
 
   <div class="row">
     <div class="col">
-      <ul class="list-group">
-        <li class="list-group-item" v-for="(todo, index) in todos" :key="index">
+      <ul class="list-group" v-if="todoStore.todos">
+        <li class="list-group-item" v-for="(todo, index) in todoStore.todos" :key="index">
           <div class="d-flex justify-content-between">
             <span :class="{ done: todo.is_complete }" @change="toggleTodo(todo)">
-              {{ todo.text }}
+              {{ todo.todo }}
             </span>
             <button type="button" class="btn btn-danger btn-sm ps-2" @click="deleteTodo(index)">
               Delete
@@ -41,9 +53,11 @@ import useTodoStore from '../stores/todoStore'
 
 const newTodo = ref('')
 const todoStore = useTodoStore()
+const errorMessage = ref('')
 
-onMounted(() => {
-  todoStore.fetchTodos()
+onMounted(async () => {
+  await todoStore.fetchTodos()
+  // todos.value.push(...todoStore.todos)
 })
 
 const addTodo = () => {
@@ -55,7 +69,15 @@ const toggleTodo = (todo) => {
   todoStore.toggleTodoCompletion(todo.id, todo.is_complete)
 }
 
-const deleteTodo = (id) => {
-  todoStore.deleteTodo(id)
+const deleteTodo = async (id) => {
+  try {
+    await todoStore.deleteTodo(id)
+  } catch (error) {
+    errorMessage.value = error.response.data.error
+  }
+}
+
+const clearErrorMessage = () => {
+  errorMessage.value = ''
 }
 </script>
