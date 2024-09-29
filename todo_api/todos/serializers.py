b0d3c7e297a3e938809d501d7f0787f0f2270f6c
Precious Chika <43546663+pre22@users.noreply.github.com>
2024-09-29 11:46:12 +0100

    created serializer for todo list

--- todo_api/todos/serializers.py
+++ todo_api/todos/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-
+from .models import TodoModel
 
 class AddTodoSerializer(serializers.Serializer):
     '''
     Create New Todo
     '''
     todo = serializers.CharField(max_length=200, required=True)
+
+class TodoListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TodoModel
+        exclude = ('user', 'modified_at', 'created_at',)
